faf821e9f3fb25e1d4489456aaaf6a53e3ab62e9
Jordane <jordane.frechet@gmail.com>
2019-12-06 16:19:39 +0100

    First commit -> Navbar and sections created

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portfolio
 Personal portfolio for Jordane Frechet
+
+Install dependencies (once) :
+npm install
+
+Start developping :
+npm start
+
+Bundle the app:
+npm run build 
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elems = document.querySelectorAll('.sidenav');
+  const instances = M.Sidenav.init(elems);
+
+  $('.sidenav-close').on('click', () => {
+    $('#slide-out').animate({
+      width: '0px',
+      'margin-left': '-300px',
+      opacity: '0',
+	  }, 500);
+	  if (window.innerWidth > 992) {
+	      $('main').animate({
+	        'margin-left': '50px',
+		  });
+    }
+  });
+
+  $('.side-nav').on('click', () => {
+    $('#slide-out').animate({
+      width: '300px',
+      'margin-left': '0',
+      opacity: '1',
+    }, 500);
+    if (window.innerWidth > 992) {
+      $('main').animate({
+        'margin-left': '300px',
+      });
+    }
+  });
+});
